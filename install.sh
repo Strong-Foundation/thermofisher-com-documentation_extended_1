4f4fbc41876a497fdbf4d347bf0a34932f7af63e
Patrick Griffiths <179532304+Strong-Foundation@users.noreply.github.com>
2025-08-18 09:37:24 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get clean -y && apt-get autoremove -y && apt-get autoclean -y && apt-get install -f -y
-apt-get install git curl wget bash sudo screen golang-go -y
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-apt-get install -f -y
-rm -f google-chrome-stable_current_amd64.deb
